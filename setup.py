f496444ab43f01d213d689e229353ac224e26bdb
kcunning <katie.fulton@gmail.com>
2009-09-16 11:24:30 -0400

    Rrrr. Git is being slow?

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+ 
+version = '0.1.0'
+
+setup(
+    name='django-glossary',
+    version=version,
+    description="django-uni-form",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='glossary,django',
+    author='Katie Cunningham',
+    author_email='katie.fulton@gmail.com',
+    url='http://github.com/pydanny/django-glossary',
+    license='MIT',
+    zip_safe=False,
+    setup_requires=['setuptools_git'],
+)
