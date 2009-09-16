6b1b58353b7e38953423624d9709f5f56700164a
kcunning <katie.fulton@gmail.com>
2009-09-16 17:30:32 -0400

    Added search!

--- glossary/views.py
+++ glossary/views.py
@@ -4,5 +4,18 @@ from django.shortcuts import render_to_response
 from glossary.models import Term
 
 def term_index(request):
+	"""
+	Return a list of all terms
+	
+	"""
 	return render_to_response('glossary/term_index.html', 
 								{ 'term_list': Term.objects.all()})
+								
+def search(request):
+	query = request.GET.get('q', '')
+	results = []
+	if query:
+		results = Term.objects.filter(description__icontains=query)
+	return render_to_response('glossary/search.html', 
+				  {'query':query,
+				  'results': results })
