22a5688c23b3b2e496c3d788515da5d870a9050f
kcunning <katie.fulton@gmail.com>
2009-09-16 15:48:22 -0400

    Oh please let this work.

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from glossary.models import Term
-
-class TermAdmin(admin.ModelAdmin):
-	prepopulated_fields = { 'slug': ['title']}
-	
-admin.site.register(Term, TermAdmin)
--- views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-
-from django.shortcuts import render_to_response
-from glossary.models import Term
-
-def term_index(request):
-	print "Oh hi!"
-	return render_to_response('glossary/term_index.html', 
-								{ 'term_list': Term.objects.all()})
