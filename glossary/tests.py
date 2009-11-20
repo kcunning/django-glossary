37075c93083b1f68c4ce53b4e4478c6cd98810de
Katie Cunningham <katie.fulton@gmail.com>
2009-11-20 11:36:28 -0500

    Added working tests! These may not work for everyone. Working on a way to de-NASAify them

--- glossary/tests.py
+++ glossary/tests.py
@@ -1,3 +1,28 @@
-import unittest
+from django.test.client import Client
+from glossary.models import Term, Synonym
+from django.test import TestCase
 
-# I have no tests here yet :(
+class GlossaryTestCase(TestCase):
+	fixtures = ["core_pages.json","glossary.json"]
+	
+	def setUp(self):
+		pass
+		
+	def tearDown(self):
+		pass
+		
+	def test_term(self):
+		self.assertTrue(Term.objects.count(), 1)
+		self.assertTrue(Synonym.objects.count(), 1)
+		
+	def test_term_view(self):
+		response = self.client.get('/glossary/')
+		self.assertTrue(response.status_code == 200)
+		
+		response = self.client.get('/glossary/aas/')
+		self.assertTrue(response.status_code == 200)
+		
+		term = Term.objects.get(slug = "aas")
+		syn = Synonym.objects.get(term = term)
+		self.assertContains(response, syn.title)
+		
