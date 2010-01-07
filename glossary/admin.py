58662d1efee67471529b4074683f6c083fb6dd4e
Chris Adams <chris.adams@nasa.gov>
2010-01-07 11:46:22 -0500

    Admin area tweaks
    Add list search to the admin page

--- glossary/admin.py
+++ glossary/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from glossary.models import Term
 
 class TermAdmin(admin.ModelAdmin):
-	prepopulated_fields = { 'slug': ['title']}
+    prepopulated_fields = { 'slug': ['title']}
+    list_display = ('title',)
+    search_fields = ('title', 'description')
 
 admin.site.register(Term, TermAdmin)
