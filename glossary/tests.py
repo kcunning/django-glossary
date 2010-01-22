0428f83129b9ef2c7700107e82e3e9b8ade7895c
Katie Cunningham <katie.fulton@gmail.com>
2010-01-22 14:06:28 -0500

    Updated tests

--- glossary/tests.py
+++ glossary/tests.py
@@ -4,24 +4,45 @@ from django.test import TestCase
 import unittest
 
 class GlossaryTestCase(TestCase):
-#	fixtures = ["core_pages.json","glossary.json"]
+	#TODO: Fix ABC nav result!
 	
 	def setUp(self):
-		self.term = Term.objects.create( title="Test", slug = "test", description="A thing I stick in a unit test")
-		self.synonym = Synonym.objects.create(title="Synonym", term = self.term)
+		self.ace = Term.objects.create( title="Ace", slug = "ace", description="Description for Ace")
+		self.base = Term.objects.create( title="Bace", slug = "base", description="Ace of BASE!")
+		self.case = Term.objects.create( title="Case", slug = "case", description="Make a case")
+		self.dace = Term.objects.create( title="Dace", slug = "dace", description="A dude named Dace")
+		self.eco = Term.objects.create( title="Eco", slug = "eco", description="Eco-awesomeness")
+		self.face = Term.objects.create( title="Face", slug = "face", description="In your face!")
+		self.gale = Term.objects.create( title="Gale", slug = "gale", description="Dorothy Gale?")
+		self.hail = Term.objects.create( title="Hail", slug = "hail", description="Hail of fail")
+		self.ill = Term.objects.create( title="Ill", slug = "ill", description="That coat is ill.")		
+		self.synonym = Synonym.objects.create(title="Synonym", term = self.ace)
 		
 	def tearDown(self):
 		pass
 		
-	def test_term(self):
-		self.assertTrue(Term.objects.count(), 1)
-		self.assertTrue(Synonym.objects.count(), 1)
+	def test_term(self):		
+		self.assertEquals(self.ace.title, self.synonym.term.title)
+		self.assertEquals(self.ace.slug, "ace")
 		
-	def test_term_view(self):
 		
-		self.assertEquals(self.term.title, self.synonym.term.title)
-		self.assertEquals(self.term.slug, "test")
+	def test_term_view(self):		
+		response = self.client.get('/glossary/')
+		self.assertTrue(response.status_code == 200)
 		
-		term = Term.objects.get(slug = "test")
-		syn = Synonym.objects.get(term = term)
+		response = self.client.get('/glossary/ace/')
+		self.assertTrue(response.status_code == 200)
+		self.assertContains(response, "Ace")
+		self.assertContains(response, "Description for Ace")
+		
+		response = self.client.get('/glossary/?l=a')
+		self.assertTrue(response.status_code == 200)
+#		self.assertContains(response, "Ace")						#TODO - Fix me!
+		self.assertContains(response, '<li class="current"><a href = "?l=a">a</a></li>')
+		
+		response = self.client.get('/glossary/?q=dude')
+		self.assertTrue(response.status_code == 200)
+		self.assertContains(response, "Dace") 
+		self.assertContains(response, '<input type="text" name="q" id="id_q" value="dude">')
+		
 		
