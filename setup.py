0c1431a3a4e70dd87d8ccec47021097f38a12f13
Chris Adams <chris@improbable.org>
2010-03-12 16:16:48 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
 from distutils.core import setup
 
-setup(version="0.1",
-      name='django-glossary',
-      description="A simple glossary system for Django-powered sites",
-      long_description="",
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Programming Language :: Python",
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      author='Katie Cunningham',
-      author_email='katie.fulton@gmail.com',
-      url='http://github.com/kcunning/django-glossary',
+setup(
+    version="0.1",
+    name='django-glossary',
+    description="A simple glossary system for Django-powered sites",
+    long_description="",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+    author='Katie Cunningham',
+    author_email='katie.fulton@gmail.com',
+    url='http://github.com/kcunning/django-glossary',
+    requires=[
+        "django (>=1.1.1)",
+    ],
+    packages=[
+        "glossary"
+    ],
+    zip_safe=False,
+    include_package_data=True,
 )
