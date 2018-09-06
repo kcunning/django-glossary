5123f9c3ca15dfd84206762115f09fdf0f6d38c6
tehfink <djsnickles@yahoo.com>
2018-09-06 15:53:04 -0400

    added namespace, cleanup

--- django_glossary/urls.py
+++ django_glossary/urls.py
@@ -1,20 +1,8 @@
 from django.conf.urls import url
 from .views import TermDetailView, TermListView
-from django_glossary.models import Term
 
 
-
-# terms = Term.objects.all()
-#
-#
-# urlpatterns = patterns('',
-#     url(r'^$',                     'glossary.views.term_list', {"paginate_by": 20}, name="glossary-list"),
-#     url(r'^(?P<slug>[-\w]+)/$',    'django.views.generic.list_detail.object_detail', {"queryset": terms}, name="glossary-detail"),
-# )
-
-
-########
-
+app_name = 'django_glossary'
 urlpatterns = [
     url(r'^$', TermListView.as_view(), name='glossary-list'),
     url(r'^(?P<slug>[-\w]+)/$', TermDetailView.as_view(), name='detail'),
